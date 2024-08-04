fab177faa81c9c57e213a5dc3c09c488e2d8c76f
Aryan-Kumr <152487056+Aryan-Kumr@users.noreply.github.com>
2024-08-04 22:52:23 +0530

    Update CustomerFrame(jdbc).java

--- CustomerFrame(jdbc).java
+++ CustomerFrame(jdbc).java
@@ -102,3 +102,129 @@ public class CustomerFrame extends JPanel {
 		});
 	}
 }
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+package jdbcProject2;
+
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class CustomerFrame extends JPanel {
+    private JLabel cNoLabel, cNameLabel, cStateLabel, cCreditLimitLabel;
+    private JTextField cNoTF, cNameTF, cStateTF, cCreditLimitTF;
+    private JButton submitButton, displayButton;
+    private JTextArea displayArea;
+
+    public CustomerFrame() {
+        // Labels
+        cNoLabel = new JLabel("Customer No:");
+        cNameLabel = new JLabel("Customer Name:");
+        cStateLabel = new JLabel("State:");
+        cCreditLimitLabel = new JLabel("Credit Limit:");
+
+        // Text fields
+        cNoTF = new JTextField(10);
+        cNameTF = new JTextField(10);
+        cStateTF = new JTextField(10);
+        cCreditLimitTF = new JTextField(10);
+
+        // Buttons
+        submitButton = new JButton("Insert");
+        displayButton = new JButton("Display");
+
+        // Layout
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        // Adding components
+        add(cNoLabel);
+        add(cNoTF);
+        add(cNameLabel);
+        add(cNameTF);
+        add(cStateLabel);
+        add(cStateTF);
+        add(cCreditLimitLabel);
+        add(cCreditLimitTF);
+        add(submitButton);
+        add(displayButton);
+
+        // Text area for displaying data
+        displayArea = new JTextArea(10, 40);
+        displayArea.setEditable(false);
+        add(displayArea);
+
+        // Insert button action listener
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo6a", "root", "password")) {
+                    Statement st = con.createStatement();
+                    int cno = Integer.parseInt(cNoTF.getText());
+                    String cuName = cNameTF.getText();
+                    String cuState = cStateTF.getText();
+                    int crLimit = Integer.parseInt(cCreditLimitTF.getText());
+
+                    String query = String.format("INSERT INTO Customer (CustNo, CustName, State, Credit_Limit) VALUES (%d, '%s', '%s', %d)", cno, cuName, cuState, crLimit);
+                    st.executeUpdate(query);
+
+                    JOptionPane.showMessageDialog(CustomerFrame.this, "Record inserted successfully");
+                } catch (SQLException se) {
+                    JOptionPane.showMessageDialog(CustomerFrame.this, "Error in connecting to database: " + se.getMessage());
+                }
+            }
+        });
+
+        // Display button action listener
+        displayButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                displayArea.setText("");
+                try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo6a", "root", "password")) {
+                    Statement st = con.createStatement();
+                    String query = "SELECT * FROM Customer";
+                    ResultSet rs = st.executeQuery(query);
+
+                    while (rs.next()) {
+                        int custNo = rs.getInt("CustNo");
+                        String custName = rs.getString("CustName");
+                        String state = rs.getString("State");
+                        int creditLimit = rs.getInt("Credit_Limit");
+                        displayArea.append(custNo + "\t" + custName + "\t" + state + "\t" + creditLimit + "\n");
+                    }
+                } catch (SQLException se) {
+                    JOptionPane.showMessageDialog(CustomerFrame.this, "Error in connecting to database: " + se.getMessage());
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Customer Management");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new CustomerFrame());
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
